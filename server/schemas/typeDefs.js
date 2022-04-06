63449becc8784f839d04e485d0789cdd7a50508d
Isaac yanez <isaac231467@gmail.com>
2022-04-06 12:03:00 -0500

    added a few files to the server side code, added the schema for mongo

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+// creating type defs
+const TypeDefs = gql`
+    type User {
+        _id: ID
+        email: String
+        password: String
+        address: String
+        allergies: [String]
+        phoneNumber: Int
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        me: User
+    }
+
+    type Mutation {
+
+    }
+
+`
+
+module.exports = TypeDefs;
