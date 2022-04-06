d4f71bb083970b555dc1a89587f1c4a8a54aacc3
Isaac yanez <isaac231467@gmail.com>
2022-04-06 18:47:29 -0500

    removed some lines of code from the connection.js file

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-// const { typeDefs, resolvers } = require('./schemas')
-// const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas')
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 // const routes = require('./routes');
@@ -11,24 +11,24 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// const startServer = async () => {
+const startServer = async () => {
     // creating new apollo server
-    // const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     context: authMiddleware
-// });
+    const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+});
 
-    // await server.start();
+    await server.start();
 
     // integrate our Apollo server with the express application as middleware
-    // server.applyMidleware({ app });
+    server.applyMiddleware({ app });
 
     // log where we can go to test our GQL API
-    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-// }
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
 
-// startServer();
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -45,11 +45,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
-  app.use(routes);
+//   app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
