ba62e47d5b618513849060cc74efa8e0489110d4
Isaac yanez <isaac231467@gmail.com>
2022-04-06 22:49:51 -0500

    commented out some code that was causing issues with the graphqllocal host

--- server/server.js
+++ server/server.js
@@ -37,13 +37,15 @@ app.use(express.json());
 
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// Will uncomment  out everything under this  once we create a build folder for production
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//   });
 
 //   app.use(routes);
 
