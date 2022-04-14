da9d24861700cdc88a60b5c0db7ff932bf10c897
chellesjord <m.kayejordan@gmail.com>
2022-04-13 22:53:56 -0500

    merge conflicts, change package json for concurrently installs

--- client/src/components/PhotoList/index.js
+++ client/src/components/PhotoList/index.js
@@ -67,36 +67,6 @@ const PhotoList = () => {
         },
     ]);
 
-<<<<<<< HEAD
-    const currentPhotos = photos.filter(photo => photo.category === category);
-
-    const toggleModal = (image, i) => {
-        setCurrentPhoto({ ...image, index: i });
-        setIsModalOpen(!isModalOpen);  
-    }
-
-    
-
-  return (
-    <div>
-      {isModalOpen && (
-        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
-      )}
-      <div className="flex-row">
-        {currentPhotos.map((image, i) => (
-          <img
-            src={require(`../../assets/food_gallery/${i}.jpg`)}
-            alt={image.name}
-            className="img-thumbnail mx-1"
-            onClick={() => toggleModal(image, i)}
-            key={image.name}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
-=======
     const toggleModal = (image, i) => {
         setCurrentPhoto({ ...image, index: i });
         setIsModalOpen(!isModalOpen);
@@ -123,6 +93,5 @@ const PhotoList = () => {
 };
 
 
->>>>>>> develop
 
 export default PhotoList;
